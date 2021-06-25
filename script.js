cd8a765b33ecd7ff3708d541aeeb6db2887bdeb9
Vetrivelan <vetri0907.vv@gmail.com>
2021-06-25 16:57:28 +0530

    project commit

--- script.js
+++ script.js
@@ -4,31 +4,41 @@ class Blog{
         this.title=title;
         this.detail=detail;
     }
+    addImage(){
+        var image = document.createElement("img");
+        image.src="./assets/java card image.svg";
+        document.getElementById("flashcard").appendChild(image);
+    }
     addTitle(){
         var title_card = document.createElement('h1');
         title_card.setAttribute("id","blog-title");
-        console.log(title_card);
         document.getElementById('card-text').appendChild(title_card);
         title_card.innerHTML += this.title;
     }
     addDescription(){
         var desc_card = document.createElement('p');
         desc_card.setAttribute("id","blog-description");
-        console.log(desc_card);
         document.getElementById('card-text').appendChild(desc_card);
         desc_card.innerHTML += this.detail;
     }
 }
 
-//event listener
+//event listeners
+
 //diplay pop up
+
 //show pop up btn
 let add_blog_btn = document.getElementById("addBlog");
-//pop up document
+//open pop up document
 let popup = document.getElementById("popupContact");
 add_blog_btn.addEventListener("click", function (){
     popup.style.display="block";
 });
+// close pop up document
+let cancelBtn = document.getElementById("close");
+cancelBtn.addEventListener("click", function (){
+    popup.style.display="none";
+})
 //post btn
 let post_btn = document.getElementById("post");
 post_btn.addEventListener("click", function (){
@@ -37,5 +47,6 @@ post_btn.addEventListener("click", function (){
     let content = new Blog(head, para);
     content.addTitle();
     content.addDescription();
+    content.addImage();
     popup.style.display="none";
 });
